bb3be49e1697578c4320ae14a1f74e0686a191e2
Shahul Khajamohideen <skhajamohid1@bloomberg.net>
2016-10-18 00:12:25 -0400

    Finish lesson 4

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('invitation');
+  },
+
+  actions: {
+    saveInvitation(newInvitation) {
+      newInvitation.save().then((response) => {
+        this.controller.set('responseMessage', `Thank you! We've just saved your email address with the following id: ${response.get('id')}`);
+        this.controller.set('model', this.store.createRecord('invitation'));
+      });
+    }
+  }
 });
